233d70514181961ddc0f6d7a3b15cfd8ab70334f
Olena Kardash <olena.kardash75@gmail.com>
2024-03-10 13:04:40 +0100

    coment 8

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -80,7 +80,14 @@ const imgElem = images.map(({ preview, original, description}) => {
 galleryContainer.insertAdjacentHTML("beforeend", imgElem);
 
 // Описаний в документації
-// // // <li class="gallery-item">
+import SimpleLightbox from "simplelightbox";
+// // Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+const lightbox = new SimpleLightbox('.gallery-link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// // <li class="gallery-item">
 // 	<a class="gallery-link" href="large-image.jpg">
 // 		<img 
 // 			class="gallery-image" 
