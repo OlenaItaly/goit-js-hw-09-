5f10dccd2b32f60f3397d4d70fdf38fc3f66fd4b
Olena Kardash <olena.kardash75@gmail.com>
2024-03-11 10:54:24 +0100

    comit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,36 +1,36 @@
 const formSet = document.querySelector('.js-form');
-const inputinfo = formSet.elements.email;
-const areainfo = formSet.elements.message;
-let saveinfo = { email: '', message: '' };
+const inputEmail = formSet.elements.email;
+const inputMessage = formSet.elements.message;
+let saveData = { email: '', message: '' };
 
-const parsedinfo = JSON.parse(localStorage.getItem('feedback-form-state'));
+const parseData = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-if (parsedinfo !== null) {
-  areainfo.value = parsedinfo.message;
-  inputinfo.value = parsedinfo.email;
+if (parseData !== null) {
+  inputMessage.value = parseData.message;
+  inputEmail.value = parseData.email;
 
-  saveinfo = parsedinfo;
+  saveData = parseData;
 }
 
 formSet.addEventListener('input', event => {
   const email = event.currentTarget.elements.email.value;
   const message = event.currentTarget.elements.message.value;
 
-  saveinfo.email = email.trim();
-  saveinfo.message = message.trim();
-  localStorage.setItem('feedback-form-state', JSON.stringify(saveinfo));
+  saveData.email = email.trim();
+  saveData.message = message.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(saveData));
 });
 
 formSet.addEventListener('submit', evt => {
   evt.preventDefault();
 
-  if (saveinfo.email.length == 0 || saveinfo.message.length == 0) {
+  if (saveData.email.length == 0 || saveData.message.length == 0) {
     console.log(`please fill all field`);
   } else {
-    console.log(saveinfo);
+    console.log(saveData);
     localStorage.removeItem('feedback-form-state');
     formSet.reset();
-    saveinfo.email = '';
-    saveinfo.message = '';
+    saveData.email = '';
+    saveData.message = '';
   }
 });
